2381584cc169b64a5a649da51f4eb7788eb6cb55
Abraarcodes <mohammedabraar360@gmail.com>
2025-09-09 17:19:17 +0530

    Login component (React hook form)

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,18 @@ import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo'
+import Button from './Button'
+import Input from './Input'
+import PostCard from './PostCard'  
+import Select from './Select'
 
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    Button,
+    Input,
+    PostCard,
+    Select
 }
